c9a5e8da2a1d0d50bcf07c0bc373232b01e47a89
flare-lee <flarelee@gmail.com>
2026-04-09 15:00:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Python is running")
+import feedparser
+
+rss_urls = {
+    "Oracle": "https://news.google.com/rss/search?q=Oracle",
+    "Wiwynn": "https://news.google.com/rss/search?q=Wiwynn+緯穎"
+}
+
+for company, url in rss_urls.items():
+    print("\n=== ", company, " ===")
+    feed = feedparser.parse(url)
+    for entry in feed.entries[:5]:
+        print("-", entry.title)
+        print(" ", entry.link)
+
